29f75b369f509981f0247659e85e4431c407e8d6
Elena2332 <elenafesional@gmail.com>
2024-10-20 21:02:36 +0200

    lista 4 hasta 7

---
+++ ia/pria/ejercicios04.py
@@ -0,0 +1,196 @@
+import math
+import datetime as dt
+
+#####  EJERCICIO 1  #####
+def ejercicio1():
+    print('Introduce numero')
+    try:
+        num = float(input())
+        # radianes = num*math.pi/180
+        radianes = math.radians(num)
+        print(num,'º  =  ',radianes,'rad')
+    except:
+        print('Error en los datos')
+    menu()
+
+
+
+#####  EJERCICIO 2  #####
+def ejercicio2():
+    try:
+        print('Introduce altura del cilindro')
+        h = float(input())
+        print('Introduce radio del cilindro')
+        radio = float(input())
+        pi = math.pi
+        rr = math.pow(radio,2)
+        volumen = pi*h*rr
+        area = 2*pi*rr + 2*pi*radio*h
+        print('Volumen: ',volumen,'\tArea: ',area)
+    except:
+        print('Error en los datos')
+    menu()
+
+
+
+
+#####  EJERCICIO 3  #####
+def ejercicio3():
+    try:
+        print('Introduce numero')
+        num = int(input())
+        suma1 = 0
+        suma2 = 0
+        for i in range(num):
+            suma1 += suma1
+            suma2 = suma2 + math.pow(i+1,2)
+        suma1 = math.pow(suma1,2)
+        print(suma1,' - ',suma2,' = ',suma1-suma2)
+    except:
+        print('Error en los datos')
+    menu()
+
+
+
+#####  EJERCICIO 4  #####
+def ejercicio4():
+    try:
+        print('Introduce numero')
+        a = int(input())
+        print('Introduce otro numero')
+        b = int(input())
+        while b != 0:
+            aux = b
+            b = a % b
+            a = aux
+        print('Maximo Comun Divisor: ',a)
+    except:
+        print('Error en los datos')
+    menu()
+
+
+
+#####  EJERCICIO 5  #####
+def ejercicio5():
+    noBien = True
+    a=b=h = 0
+    while noBien:
+        try:
+            print('''Calculadora de Pitagoras
+                   Introduce 0 si es lo que necesitas calcular
+                   Introduce cateto a''')
+            a = float(input())
+            print('Introduce cateto b')
+            b = float(input())
+            print('Introduce hipotenusa')
+            h = float(input())
+            if a==0 or b==0 or h==0:
+                noBien = False
+        except:
+            print('Error en los datos')
+    # h^2 = a^2 + b^2
+    if h==0:  # calcular hipotenusa
+        h = math.sqrt(math.pow(a,2)+math.pow(b,2))
+    elif a==0:
+        a = math.sqrt(math.pow(h,2)-math.pow(b,2))
+    elif b==0:
+        b = math.sqrt(math.pow(h,2)-math.pow(a,2))
+    print('Hipotenusa: {}, cateto a: {}, cateto b: {}'.format(h,a,b))
+    menu()
+
+
+#####  EJERCICIO 6  #####
+def ejercicio6():
+    print('''
+          a) Fecha y hora actuales
+          b) Año en curso
+          c) Mes del año
+          d) Número de semana del año
+          e) Día laborable de la semana
+          f) Día del año
+          g) Día del mes
+          h) Día de la semana''')
+    resp = input()
+    d = dt.datetime.now()
+    match resp:
+        case 'a':
+            print('Fecha y hora: ',d.strftime('%d/%m/%y   %X'))
+        case 'b':
+           print("Año: ", d.year)
+        case 'c':
+           print("Mes: ", d.strftime('%B'))
+        case 'd':
+            print('Numero de semana del año: ',d.strftime('%W'))
+        case 'e':
+            print('Dia laborable: ', d.strftime('%u'))
+        case 'f':
+            print('Dia del año: ', d.strftime('%j'))
+        case 'g':
+           print("Dia del mes: ", d.strftime('%d de %B'))
+        case 'h':
+            print('Dia de la semana: ', d.strftime('%A'))
+        case _:
+            print('Respuesta incorrecta')
+    menu()
+
+
+#####  EJERCICIO 7  #####
+def ejercicio7():
+    d = dt.datetime.now()
+    print('Fecha actual: ',d.strftime('%d-%m-%y'))
+    print('Fecha 5 dias antes: ', )
+    menu()
+
+
+#####  EJERCICIO 8  #####
+def ejercicio8():
+    menu()
+
+
+#####  EJERCICIO 9  #####
+def ejercicio9():
+    menu()
+
+
+
+#### MENU ####
+def menu():
+    try:
+        print('''\nSelecciona Ejercicio
+            1 = Ejercicio1
+            2 = Ejercicio2
+            3 = Ejercicio3
+            4 = Ejercicio4
+            5 = Ejercicio5
+            6 = Ejercicio6
+            7 = Ejercicio7
+            8 = Ejercicio8
+            9 = Ejercicio9
+            10 = Ejercicio10
+            para TERMINAR cualquier otra cosa''')
+        num = int(input())
+        if num == 1 : 
+            ejercicio1()
+        elif num == 2:
+           ejercicio2()
+        elif num == 3:
+           ejercicio3()
+        elif num == 4:
+           ejercicio4()
+        elif num == 5:
+           ejercicio5()
+        elif num == 6:
+           ejercicio6()
+        elif num == 7:
+           ejercicio7()
+        elif num == 8:
+           ejercicio8()
+        elif num == 9:
+           ejercicio9() 
+        # elif num == 10:
+        #    ejercicio10()  
+        else:
+            print('Adios')
+    except ValueError:
+        print('Adios')
+menu()
