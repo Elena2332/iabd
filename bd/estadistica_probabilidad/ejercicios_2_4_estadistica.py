bd050990cccfefa2e0fd99dab892bf8d04620f4f
Elena2332 <elenafesional@gmail.com>
2024-11-04 15:57:34 +0100

    reorganizacion parte 2

--- bd/estadistica_probabilidad/ejercicios_2_4_estadistica.py
+++ bd/estadistica_probabilidad/ejercicios_2_4_estadistica.py
@@ -9,11 +9,14 @@ def ejercicio1():
     prob_mas_2m = stats.norm.cdf(2, media, desviacion)
     prob_menos_1_85m = stats.norm.cdf(1.85, media, desviacion)
     #prob_mas_2m = stats.norm.cdf(2, media, desviacion)
+    
+    print('1.Probabilidad de medir más de 2m: ',prob_mas_2m)
+    print('2.Probabilidad de medir menos de 1.85m: ',prob_menos_1_85m)
     menu()
 
 
 
-def ejercicio11():
+def ejercicio2():
     
     menu()
 
@@ -23,13 +26,13 @@ def menu():
     try:
         print('''\nSelecciona Ejercicio
             1 = Ejercicio1
-            2 = Ejercicio11
+            2 = Ejercicio2
             para TERMINAR cualquier otra cosa''')
         num = int(input())
         if num == 1 : 
             ejercicio1()
-        elif num == 11 or num == 2:
-            ejercicio11()
+        elif num == 2:
+            ejercicio2()
         else:
             print('Adios')
     except ValueError:
