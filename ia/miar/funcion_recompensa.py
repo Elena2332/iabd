e5f28299a4b8d50b4d86c3454b3be66f92b9aa5e
Elena2332 <elenafesional@gmail.com>
2024-11-06 18:27:37 +0100

    commit

--- ia/miar/funcion_recompensa.py
+++ ia/miar/funcion_recompensa.py
@@ -25,33 +25,49 @@
 }'''
 
 def reward_function(params):
-    
+    reward = 0
+
     # Read input parameters
     track_width = params['track_width']
     distance_from_center = params['distance_from_center']
-    reward = 0
+    abs_steering = abs(params['steering_angle']) # Only need the absolute steering angle
     
     # Calculate 3 markers that are at varying distances away from the center line
+    marker_1 = 0.1 * track_width
     marker_2 = 0.25 * track_width
     marker_3 = 0.5 * track_width
     
     # Give higher reward if the car is closer to center line and vice versa
-    if distance_from_center <= marker_2:
-        reward += 0.6
+    if distance_from_center <= marker_1:
+        reward = 1.0
+    elif distance_from_center <= marker_2:
+        reward = 0.5
     elif distance_from_center <= marker_3:
-        reward += 0.2
+        reward = 0.1
     else:
-        reward += 0
+        reward = 1e-3  # likely crashed/ close to off track
 
+
+    #Zig-zag
+    # Steering penality threshold, change the number based on your action space setting
+    ABS_STEERING_THRESHOLD = 15 
+    
+    # Penalize reward if the car is steering too much
+    if abs_steering > ABS_STEERING_THRESHOLD:
+        reward *= 0.8
+
+
+    #salida de pista
     # recompensa por mantenerse dentro del recorrido
     if params['all_wheels_on_track']:
-        reward += 0.03
+        reward += 0.3
 
     # si se ha salido penalizamos
     if params['is_offtrack']:
-        reward += 1e-3  
+        reward += -2 
 
-    # recompensamos por llegar al 25,50,75 y 95 % del circuito
+
+    # recompensamos segun progreso del circuito
     progress = round(params['progress'],2)
     if progress == 0.25:
         reward += 1
@@ -63,6 +79,5 @@ def reward_function(params):
         reward += 4
     else:
         reward += 0
-    
     
     return float(reward)
