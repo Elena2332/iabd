86b5083bf387fbeb53a2fd0d52fc72c178ed0160
Elena2332 <elenafesional@gmail.com>
2024-11-14 16:57:00 +0100

    2_4 sapa terminado(esperemos)

--- ia/miar/funcion_recompensa.py
+++ ia/miar/funcion_recompensa.py
@@ -26,35 +26,33 @@
 
 def reward_function(params):
     reward = 0
-
     # Read input parameters
     track_width = params['track_width']
     distance_from_center = params['distance_from_center']
     abs_steering = abs(params['steering_angle']) # Only need the absolute steering angle
-    
     # Calculate 3 markers that are at varying distances away from the center line
     marker_1 = 0.1 * track_width
     marker_2 = 0.25 * track_width
     marker_3 = 0.5 * track_width
-    
+   
     # Give higher reward if the car is closer to center line and vice versa
     if distance_from_center <= marker_1:
-        reward = 1.0
+        reward = 1.1
     elif distance_from_center <= marker_2:
-        reward = 0.5
+        reward = 0.4
     elif distance_from_center <= marker_3:
         reward = 0.1
     else:
-        reward = 1e-3  # likely crashed/ close to off track
+        reward = 1e-5  # likely crashed/ close to off track
 
 
     #Zig-zag
     # Steering penality threshold, change the number based on your action space setting
-    ABS_STEERING_THRESHOLD = 15 
-    
+    ABS_STEERING_THRESHOLD = 15
+   
     # Penalize reward if the car is steering too much
     if abs_steering > ABS_STEERING_THRESHOLD:
-        reward *= 0.8
+        reward *= 0.7
 
 
     #salida de pista
@@ -64,7 +62,7 @@ def reward_function(params):
 
     # si se ha salido penalizamos
     if params['is_offtrack']:
-        reward += -2 
+        reward += -2
 
 
     # recompensamos segun progreso del circuito
@@ -79,5 +77,5 @@ def reward_function(params):
         reward += 4
     else:
         reward += 0
-    
+   
     return float(reward)
---
+++ ia/sapa/2_4Presentacion_solucion.py
@@ -0,0 +1,94 @@
+import numpy as np
+import pandas as pd
+import joblib
+import os
+from sklearn.impute import SimpleImputer
+
+# metodos pipeline
+def familia_name(function_transformer, feature_names_in):
+     return ['familia']
+def familia_pipeline(X):
+     X['sibsp'].fillna(X['sibsp'].mode()[0], inplace=True) 
+     X['parch'].fillna(X['parch'].mode()[0], inplace=True) 
+     X['familia'] = X['sibsp'] + X['parch'] 
+     #X=X.drop(['sibsp','parch'],axis=1)
+     return X['familia'].values.reshape(-1,1)
+
+def age_name(function_transformer, feature_names_in):
+     return ['age']
+def separar_age(X):
+     imp = SimpleImputer(strategy='mean')
+     X_age_imp = imp.fit_transform(X)
+     X = pd.cut(X_age_imp.flatten(), bins=[-1,16,32,48,64,np.inf], labels=[1,2,3,4,5]).to_numpy().reshape(-1,1)  #labels no admite str
+     #X=X.drop('age',axis=1)  #sobra
+     return X
+
+def sex_name(function_transformer, feature_names_in):
+     return ['sex']
+def pasar_sex(X):
+     return np.where(X == 'female',1,0)
+
+
+
+# cargar el CSV
+def cargar_csv():
+    noBien=True
+    while noBien:
+        ruta = input('Introduce ruta del CSV ')
+        if os.path.isfile(ruta):
+            try:
+                df = pd.read_csv(ruta)
+                # quitamos survived si esta
+                if 'survived' in df.columns:
+                    df.drop(labels=['survived'], axis=1)
+            except Exception as e:
+                print('Error al cargar el archivo: ', e)
+            noBien = False
+        else:
+            print('Archivo inexistente, comprueba la ruta: ',ruta)
+    return df
+
+
+# cargar el modelo, devuelve None en caso de error
+def cargar_modelo(ruta):
+    ruta = input('Introduce ruta del modelo (archivo.pkl)')
+    if os.path.isfile(ruta):
+        try:
+            modelo = joblib.load(ruta)
+            return modelo
+        except Exception as e:
+            print('Error al cargar el modelo: ',e)
+            return None
+    else:
+        print('Modelo inexiste, comprueba la ruta: ',ruta)
+        return None
+
+
+# hacer la predicción, devuelve None en caso de error
+def predecir(modelo, df):
+    try:
+        prediciones = modelo.predict(df)
+        return prediciones
+    except Exception :
+        return None
+
+
+
+
+
+#####  MAIN  #####
+# cargar del CSV
+df = cargar_csv()
+
+# cargar el modelo
+modelo = cargar_modelo()
+
+# predecir si el modelo no es None
+if modelo is not None:
+    predicciones = predecir(modelo, df)
+    if predicciones is not None:    # Mostrar las predicciones
+        df['predicciones'] = predicciones
+        print('Predicciones realizadas:\n', df[['predicciones']])
+    else:
+        print('Error al hacer las predicciones.')
+
