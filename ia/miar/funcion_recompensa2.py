c256da42cb5d564bfb8ea9e08e588a234124a528
Elena2332 <elenafesional@gmail.com>
2024-11-20 18:12:53 +0100

    recompensa35 por hacer. Revisar parametros de giro

--- ia/miar/funcion_recompensa2.py
+++ ia/miar/funcion_recompensa2.py
@@ -1,4 +1,4 @@
-#########  recompensa-34  ###########
+#########  recompensa-35  ###########
 def reward_function(params):
     reward = 0
     # Read input parameters
@@ -6,6 +6,7 @@ def reward_function(params):
     distance_from_center = params['distance_from_center']
     abs_steering = abs(params['steering_angle']) # Only need the absolute steering angle
     speed = params['speed']
+    ### Centrado
     # Calculate 3 markers that are at varying distances away from the center line
     marker_1 = 0.1 * track_width
     marker_2 = 0.25 * track_width
@@ -21,24 +22,24 @@ def reward_function(params):
     else:
         reward = 1e-3  # likely crashed/ close to off track
 
-    #Zig-zag
+    ###Zig-zag
     # Steering penality threshold, change the number based on your action space setting
     ABS_STEERING_THRESHOLD = 15
    
     # Penalize reward if the car is steering too much
     if abs_steering > ABS_STEERING_THRESHOLD:
         reward *= 0.6
+    ### Velocidad 
     else: 
         if speed < 1.8:   # penaliza si lento en las rectas
             reward -= (speed - 1.8) * 0.2 
     if speed > 1.5:  # premia si va mas rapido
         reward += (speed - 1.5) * 0.3  # Increase reward based on speed
 
-    #salida de pista
+    ### Salida de pista
     # recompensa por mantenerse dentro del recorrido
     if params['all_wheels_on_track']:
         reward += 0.2
-
     # si se ha salido penalizamos
     if params['is_offtrack']:
         reward += -2
