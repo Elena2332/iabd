30afdaffd702bf61150410c731f003db0bb88245
Elena2332 <elenafesional@gmail.com>
2024-12-08 23:35:22 +0100

    apuntes pandas y docu deeprace

--- ia/pria/teoria/0_camino.py
+++ ia/pria/teoria/0_camino.py
@@ -50,7 +50,6 @@ np.divide(np_arr, np_arr)  # divide
 np.divmod(np_arr, np_arr)  # divide, devuelve 2 tuplas cociente y resto
 np.power(np_arr, np_arr)  # primero elevado al segundo
 
-
 np_fun = np.frompyfunc(funcion, 1, 2)  # funcion: funcion que se ejecuta para cada valor, 1:parametros, 2:returns
 np_fun(np_arr)
 
@@ -63,11 +62,34 @@ M.dot(M)  # multiplicar o producto matricial
 
 
 #######  PANDAS  ########
+# crear
+datos = {}
+df = pd.DataFrame(data=datos)
+pd.DataFrame(data=datos, index =['id_fila1','id_fila2'])
+pd.DataFrame(data=[[1,2],[1,2]], columns=['x','y'])  # data: lista de listas o resultado de zip()
+df.copy()
+
+# acceso valores
+df.add()  # aniadir
+df.columns  # array nombre de columnas
+df['']  # valores de una columna Serie
+df.loc['']  # nombres de columnas
+df.iloc[0]  # indices columnas
+df[df.loc['c1':'c3']]  # columnas desde c1 a c3
+df[df.iloc[1:3]]  # desde la primera columna a la tercera
 
 
+# metodos
+df.head()  # 5 primeras
+df.tail()  # 5 ultimas
+df.rank(ascending=True)  # ranking, ascending ordena(opcional)
+df.nunique()  # conteo valores unicos por columna
+df.drop_duplicates(subset='nom_col', keep=False)  # keep first: primero unico reto duplicados, last, false:todos duplicados
 
+df.insert(loc=3, column='nom_col', value=['v1','v2'])  # inserta columna con valores, loc:posicion(si ya existe el resto se mueven a la derch)
+df.insert(3, 'nom_col', 'a')  # 3: loc, 'a' es el valor para todas las filas
 
-
+df.drop(labels=['c1'], axis=1)  # axis: 0-fila 1-columna, labels: lo que se borra
 
 
 
